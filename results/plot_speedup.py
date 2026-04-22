fd7beb5fe9921f56506cb7531263fe45699c1479
KLizzy-code <kayshahiciano@gmail.com>
2026-04-21 20:59:33 -0400

    Update plot_speedup.py

--- results/plot_speedup.py
+++ results/plot_speedup.py
@@ -1,17 +1,13 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 
-# Leer datos (ajusta la ruta)
 df = pd.read_csv("../results/times_auto.csv")
 
-# Calcular speed-up
 T1 = df['time'].iloc[0]
 df['speedup'] = T1 / df['time']
 
-# Mostrar tabla
 print(df)
 
-# Graficar
 plt.figure()
 plt.plot(df['cores'], df['speedup'], marker='o')
 plt.xlabel("Número de cores")
@@ -19,7 +15,6 @@ plt.ylabel("Speed-up")
 plt.title("Strong Scaling - Modelo SIR")
 plt.grid()
 
-# Guardar imagen
 plt.savefig("speedup.png")
 
 plt.show()
